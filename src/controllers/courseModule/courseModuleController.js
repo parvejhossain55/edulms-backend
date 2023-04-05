b974cdc883c662796b6c006209ab497a813b2b3c
develpoeralamgirbd <developer.alamgirbd@gmail.com>
2023-04-05 20:55:43 +0600

    course module

---
+++ src/controllers/courseModule/courseModuleController.js
@@ -0,0 +1,7 @@
+const createModule = async (req, res, next)=>{
+    try {
+
+    }catch (e) {
+        next(e)
+    }
+}
--- src/models/CourseContent.js
+++ src/models/CourseContent.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose');
 
 const courseContentSchema = new Schema({
-    courseModuleId: {
+    moduleId: {
         type: Schema.Types.ObjectId,
         ref: 'CourseModule'
     },
