aa30061710943d020ba71f8fe7892d3a8753107c
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-05-22 12:37:36 +0600

    assignment create validation done

--- src/services/assignment/assignment.services.js
+++ src/services/assignment/assignment.services.js
@@ -9,15 +9,14 @@ const createAssignmentService = async (
     {assignmentName, assignmentDescription, courseId, courseModuleId, filename, teacherId}
 )=>{
 
-    const isAssignment = await AssignmentModel.findOne({courseId: new objectId(courseId), courseModuleId: new objectId(courseModuleId), teacherId: new objectId(teacherId)});
+    const isAssignment = await AssignmentModel.findOne({courseId: new objectId(courseId), courseModuleId: new objectId(courseModuleId), teacherId: teacherId});
     if (isAssignment){
-        error("assignment already exit in this course module", 400)
+       throw error("assignment already exit in this course module", 400)
     }
-    console.log(isAssignment)
 
     const isCourse = await checkAssociateService({teacherId: new objectId(teacherId), _id: new objectId(courseId)}, CourseModel);
     if (!isCourse){
-        error("course not found. please create a course first", 400)
+        throw error("course not found. please create a course first", 400)
     }
 
     const assignment = new AssignmentModel({
